f979364b36b56c30dd9ca1d7c126d3036d8214a4
Satwik-pal-git <satwikpal01@gmail.com>
2024-02-01 21:39:53 +0530

    Updating carousel work

--- src/Pages/LandingPage/FeaturedProject.jsx
+++ src/Pages/LandingPage/FeaturedProject.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import Card from "../../common/Post";
+import Post from "../../common/Post";
+import ReactCardCarousel from "react-card-carousel";
+
 
 function FeaturedProject() {
     return (
-        <></>
+        <div className='relative grid grid-row gap-8 w-[100%] h-[80%]'>
+            <div className="flex items-center justify-center text-3xl font-bold">
+                Our Featured Project
+            </div>
+            <div className="relative" >
+                <ReactCardCarousel autoplay={false} autoplay_speed={2500}>
+                    <div className=''>
+                        <Post />
+                    </div>
+                    <div>
+                        <Post />
+                    </div>
+                    <div>
+                        <Post />
+                    </div>
+                </ReactCardCarousel>
+            </div>
+        </div>
     );
 }
 
