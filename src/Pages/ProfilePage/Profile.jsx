0d7c910e81fd065bef2096e061598cc74d7bc435
Satwik-pal-git <satwikpal01@gmail.com>
2024-03-29 01:07:42 +0530

    profle data updated

--- src/Pages/ProfilePage/Profile.jsx
+++ src/Pages/ProfilePage/Profile.jsx
@@ -3,11 +3,13 @@ import { MdOutlineEdit } from "react-icons/md";
 import { FaUserCircle } from "react-icons/fa";
 import Sidebar from "./Sidebar";
 import MobileSidebar from "./mobileSidebar";
+import { useAuth } from "../../utils/AuthContext";
 
 const Profile = () => {
   const [isEditing, setIsEditing] = useState(false);
-  const [tabContent, setTabContent] = useState("");
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const { user } = useAuth();
+  const [tabContent, setTabContent] = useState({ data: user, title: "ABOUT", linkId: "" });
 
   const handleIncomingData = (data, title, linkId) => {
     setTabContent({ data, title, linkId });
@@ -15,6 +17,18 @@ const Profile = () => {
   const sidebarHandler = () => {
     setSidebarOpen(!sidebarOpen);
   };
+  let date = new Date(user.createdAt);
+  let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let day = date.getUTCDate().toString().padStart(2, '0');
+  let month = monthNames[date.getUTCMonth()];
+  let year = date.getUTCFullYear().toString().slice(2);
+  const dateOfJoined = `${day} ${month} ${year}`;
+  date = new Date(user.dob);
+  monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  day = date.getUTCDate().toString().padStart(2, '0');
+  month = monthNames[date.getUTCMonth()];
+  year = date.getUTCFullYear().toString().slice(2);
+  const DOB = `${day} ${month} ${year}`;
 
   return (
     <div className="grid font-['Playfair-Display'] grid-cols-12">
@@ -50,19 +64,14 @@ const Profile = () => {
         <div className="vsm:p-[10px] vsm:ml-[10px] vsm:mt-[45px]  sm:w-[100%] sm:mt-[45px] sm:ml-[10px] sm:p-[15px] md:mt-[50px] md:ml-[60px] md:p-[20px] md:w-[85%] lg:mt-[50px] lg:ml-[100px] lg:w-[80%] lg:p-4 xl:w-[80%] xl:p-[20px] xl:ml-[100px] bg-[#f1f1f8] rounded-lg shadow-md  relative">
           <div className="flex items-center">
             <div className="flex-shrink-0">
-              {isEditing ? (
-                <div className="flex flex-col">
-                  <FaUserCircle
-                    variant="circular"
-                    size="6x"
-                    alt="user"
-                    className="border border-gray-900 p-0.5 rounded-full"
-                  />
+              {user.image ? (
+                <div>
+                  <img src={user.image} alt="user" className="border border-gray-900 p-0.5 vsm:w-[60px] vsm:h-[60px] sm:w-[80px] sm:h-[80px] md:w-[100px] md:h-[100px] lg:w-[120px] lg:h-[120px] xl:w-[150px] xl:h-[150px] rounded-full" />
                 </div>
               ) : (
                 <FaUserCircle
                   variant="circular"
-                  size="6x"
+                  size="100px"
                   alt="user"
                   className="border border-gray-900 p-0.5 vsm:w-[60px] vsm:h-[60px] sm:w-[80px] sm:h-[80px] md:w-[100px] md:h-[100px] lg:w-[120px] lg:h-[120px] xl:w-[150px] xl:h-[150px] rounded-full"
                 />
@@ -70,10 +79,10 @@ const Profile = () => {
             </div>
             <div className=" vsm:ml-3 sm:ml-4  md:ml-8 lg:ml-8">
               <h2 className="vsm:text-[14px] sm:text-[15px] text-purple-800 md:text-[17px] lg:text-[19px] xl:text-[23px]  font-bold flex flex-start">
-                User Name
+                Welcome {user.name}
               </h2>
               <p className="text-purple-500 vsm:text-[11px] sm:text-[12px] md:text-[12px] lg:text-[15px] xl:text-[18px]">
-                User ID
+                Email address: {user.email}
               </p>
               {isEditing ? (
                 <input
@@ -133,7 +142,29 @@ const Profile = () => {
                   {tabContent.title}
                 </h2>
                 <div className="vsm:text-[15px] text-white vsm:mt-[10px] sm:mt-[12px] sm:text-[14px] md:p-[15px] lg:p-[15px] md:text-[15px] lg:text-[16px] lg:mt-[15px] xl:text-[18px] xl:mt-[15px] text-pretty items-center text-left ">
-                  {tabContent.data}
+                  <table class="table-auto">
+                    <tbody>
+                      <tr>
+                        <td className="pr-6">Name: </td>
+                        <td>{user.name}</td>
+                      </tr>
+                      <tr>
+                        <td className="pr-6">Email address: </td>
+                        <td>{user.email}</td>
+                      </tr>
+                      <tr>
+                        <td className="pr-6">Location: </td>
+                        <td>{user.location}</td>
+                      </tr>
+                      <tr>
+                        <td className="pr-6">Date of birth: </td>
+                        <td>{DOB}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="">
+                    Member since {dateOfJoined}
+                  </div>
                 </div>
               </div>
             )}
