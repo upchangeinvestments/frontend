243bb616132ed7a65bd737e1a35e1b4a85bab360
ShlokV <shlok.verma2011@gmail.com>
2024-04-09 14:05:33 -0700

    responsive for landing page

--- src/Pages/LandingPage/Tagline.jsx
+++ src/Pages/LandingPage/Tagline.jsx
@@ -5,7 +5,8 @@ function Tagline() {
   // const words = ["INVESTMENTS", "COMPANIES", "INVESTMENTS", "COMPANIES"];
 
   return (
-    <div className="flex  vsm:flex-col md:flex-row v md:w-full md:gap-2 items-center justify-center text-3xl	text-nowrap overflow-y-hidden">
+    <div>
+    <div className="lg:flex  vsm:flex-col vsm:hidden  md:flex-row v md:w-full  md:gap-2 items-center justify-center text-3xl	text-nowrap overflow-y-hidden">
       <div className="font-['Playfair-Display']  ">
       Explore your next Investment:  <span className="text-white"> Infinite opportunities</span>
       </div>
@@ -26,6 +27,32 @@ function Tagline() {
           </span>
         </div> */}
       </div>
+    </div>
+    <div className="vsm:flex  vsm:flex-col lg:hidden items-center justify-center vsm:text-3xl md:text-4xl">
+      <div className="font-['Playfair-Display']  ">
+      Explore your next Investment: 
+      </div>
+      <div>
+      <span className="text-white"> Infinite opportunities </span>under
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="">
+         one <span className="text-white">convenient platform.</span>
+        </div>
+
+        {/* <div className="scroller ">
+          <span className="text-left font-['Playfair-Display']  ml-2 text-white">
+            {words[0]}
+            <br />
+            {words[1]}
+            <br />
+            {words[2]}
+            <br />
+            {words[3]}
+          </span>
+        </div> */}
+      </div>
+    </div>   
     </div>
   );
 }
