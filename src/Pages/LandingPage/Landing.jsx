f0f5141683cd6bb95232f9a63a92345b14c58a4f
ShlokV <shlok.verma2011@gmail.com>
2024-04-10 19:07:36 +0530

    changes in landing and welcome page

--- src/Pages/LandingPage/Landing.jsx
+++ src/Pages/LandingPage/Landing.jsx
@@ -13,19 +13,19 @@ function LandingPage() {
   return (
     <div className="relative vsm:h-[102vh] md:h-[95vh] lg:h-[90vh] w-full flex flex-col justify-center items-end Playfair-Display  vsm:flex vsm:flex-col vsm:items-center vsm:justify-center">
       <div
-        className="absolute vsm:mx-auto md:right-20 md:top-32 lg:right-[3vw] lg:top-28 xl:-right-[1vw] xl:top-32 2xl:top-72 2xl:right-60"
+        className="absolute vsm:ml-[227px]   md:right-[-200px] md:top-32 lg:right-[-200px] lg:top-28 xl:-right-[1vw] xl:top-32 2xl:top-72 2xl:right-60"
         data-aos="zoom-in-up"
       >
         {/* <div className="absolute vsm:mx-auto md:right-4 md:top-32 md:mr-12 lg:mr-[9vw] lg:top-28 xl:mr-36 xl:mt-20 2xl:mt-32 2xl:mr-72"> */}
         <div className="mb-4 font-bold vsm:text-3xl xl:text-4xl 2xl:text-5xl drop-shadow-lg	">
           Centralized Platform For
         </div>
-        <div className="mb-8 text-white font-bold vsm:text-4xl xl:text-4xl 2xl:text-5xl drop-shadow-lg vsm:w-[37rem] vsm:h-[1.2em] vsm:overflow-y-hidden">
+        <div className="mb-8 text-white font-bold vsm:text-3xl xl:text-4xl 2xl:text-5xl drop-shadow-lg vsm:w-[37rem] vsm:h-[1.2em] vsm:overflow-y-hidden">
           <div className="">
             <p className="font-['Playfair-Display'] "> Real Estate</p>
           </div>
           <div className="scroller">
-            <span className="text-left font-['Playfair-Display'] -ml-[110px] text-white">
+            <span className="text-left font-['Playfair-Display']  vsm:-ml-[135px] xl:-ml-[110px] text-white">
               {words[0]}
               <br />
               {words[1]}
