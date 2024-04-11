19631dcdd51a9478e52d39081b158e5a7a4fca93
ShlokV <shlok.verma2011@gmail.com>
2024-04-12 02:51:14 +0530

    added how it works page

--- src/commonComponents/WaitListModal.jsx
+++ src/commonComponents/WaitListModal.jsx
@@ -148,7 +148,7 @@ function DialogDefault({ isOpen, setOpen }) {
                                     <button className="mt-6 bg-top vsm:px-4 vsm:py-1  lg:py-1 rounded-md md:text-base lg:text-base  xl:text-lg text-black font-bold  bg-cover " style={{ backgroundImage: `url(${bgImage})` }}>
                                         Verify & Join Waitlist
                                     </button>
-                                    <Tooltip title="Check spam if email not recieved">
+                                    <Tooltip title="Check spam if email not recieved" >
                                       <Button ><CiCircleQuestion className="text-3xl text-[#fff] mt-6" /></Button>
                                     </Tooltip>     
                                 </div>
