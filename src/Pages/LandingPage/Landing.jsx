4f6940a78fa9481f08e74862edd3b251247bc23b
Satwik-pal-git <satwikpal01@gmail.com>
2024-05-21 20:35:42 +0530

    added animation in the landing page heading

--- src/Pages/LandingPage/Landing.jsx
+++ src/Pages/LandingPage/Landing.jsx
@@ -7,31 +7,16 @@ import "../../styles/LandingPage/LandingPage.css";
 import { useAuth } from "../../utils/AuthContext"
 
 function LandingPage() {
-  const [orientation, setOrientation] = React.useState('');
   const words = ["Investments", "Opportunites", "Investments", "Opportunites", "Investments"];
   const { isAuth } = useAuth();
 
-  const mediaQuery = window.matchMedia('(orientation: portrait)');
   useEffect(() => {
-    const listener = (event) => {
-      if (event.matches) {
-        setOrientation('portrait');
-      } else {
-        setOrientation('landscape');
-      }
-    };
-
-    mediaQuery.addListener(listener);
-    listener(mediaQuery);
-
-    return () => {
-      mediaQuery.removeListener(listener);
-    };
-  }, [mediaQuery]);
+    Aos.init();
+  }, []);
 
   return (
     <div className="relative vsm:h-[85vh] md:h-[95vh] lg:h-[90vh] w-full flex flex-col justify-center items-center Playfair-Display">
-      <div className="relative flex items-center justify-center lg:ml-[50%] lg:-mt-[20%]">
+      <div className="relative flex items-center justify-center lg:ml-[50%] lg:-mt-[20%]" data-aos="zoom-in-up">
         <div className="font-bold vsm:text-2xl md:text-3xl xl:text-4xl 2xl:text-5xl drop-shadow-lg font-['Playfair-Display'] ">
           <div className="mb-2">
             Centralized Platform For
