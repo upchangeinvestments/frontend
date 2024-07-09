39a9b64dd37feb763db7a419ab5efde9494492a6
Satwik-pal-git <satwikpal01@gmail.com>
2024-07-10 02:26:26 +0530

    landing page video issue solved

--- src/Pages/LandingPage/Landing.jsx
+++ src/Pages/LandingPage/Landing.jsx
@@ -1,23 +1,20 @@
 import React, { useEffect } from "react";
-import ExploreMoreButton from "../../commonComponents/exploreMoreButton";
-import Tagline from "./Tagline";
-import Aos from "aos";
-import "aos/dist/aos.css";
 import "../../styles/LandingPage/LandingPage.css";
 import BackgroundVideo from "../../assets/bgVideo.mp4"
 
 function LandingPage() {
   const words = ["Investments", "Opportunites", "Firms", "Investments", "Opportunites", "Firms", "Investments"];
 
-  useEffect(() => {
-    Aos.init();
-  }, []);
-
   return (
-    <div className={`relative vsm:h-[85vh] md:h-[95vh] ${navigator.userAgent.indexOf('iPad') > -1 ? 'lg:h-[85vh]' : 'lg:h-[90vh]'} w-full flex flex-col justify-center items-center Playfair-Display `}>
-      <div className="mainBgVideo">
-        <video className='md:-mt-[150px] w-full h-full object-cover' autoPlay muted playsInline>
-          <source className="BGVIDEO w-full h-full" src={BackgroundVideo} type='video/mp4' />
+    <div className={`relative vsm:h-[40vh] md:h-[95vh] ${navigator.userAgent.indexOf('iPad') > -1 ? 'lg:h-[85vh]' : 'lg:h-[90vh]'} w-full flex flex-col justify-center items-center Playfair-Display `}>
+      <div className="md:hidden -mt-[50px] w-[120%] h-[100%]">
+        <video className="video h-[110%]" autoPlay playsInline muted>
+          <source src={BackgroundVideo} type="video/mp4" />
+        </video>
+      </div>
+      <div className="hidden md:flex items-center justify-center -mt-[150px]">
+        <video className="" autoPlay playsInline muted>
+          <source src={BackgroundVideo} type="video/mp4" />
         </video>
       </div>
     </div>
