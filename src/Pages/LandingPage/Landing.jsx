68fc7f080374bc25db5b2093d50d5058f388bb7d
Satwik-pal-git <satwikpal01@gmail.com>
2025-06-25 01:51:39 +0530

    image updated in the landing page

--- src/Pages/LandingPage/Landing.jsx
+++ src/Pages/LandingPage/Landing.jsx
@@ -10,7 +10,7 @@ function LandingPage() {
   return (
     <div className="flex flex-row m-4 mb-12 w-[90%] mx-auto">
       <div className="basis-1/2 bordr-2 border-black">
-        <img className="mx-auto rounded-[2rem] w-[85%]" src="https://sdmntprwestcentralus.oaiusercontent.com/files/00000000-697c-61fb-82ae-6f21e286779f/raw?se=2025-06-24T19%3A44%3A35Z&sp=r&sv=2024-08-04&sr=b&scid=090f56d9-50e0-5e3b-9b19-12f3a898f7e6&skoid=e9d2f8b1-028a-4cff-8eb1-d0e66fbefcca&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-06-24T14%3A17%3A38Z&ske=2025-06-25T14%3A17%3A38Z&sks=b&skv=2024-08-04&sig=vHSRkAFjgEqD1KtptJCeXsikd6ORFBhXv1QJCkX2qMs%3D" alt="" />
+        <img className="mx-auto rounded-[2rem] w-[85%]" src="https://i.postimg.cc/TPChFK12/temp.png" alt="" />
       </div>
       <div className="basis-1/2 bordr-2 border-black m-auto ">
         <div class="max-w-lg mx-auto p-12 rounded-[2rem] bg-gradient-to-br from-[#1f0036] to-[#3b0a72] text-white shadow-lg">
